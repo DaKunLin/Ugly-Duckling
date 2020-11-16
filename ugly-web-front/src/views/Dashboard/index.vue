127e69ae6b1ba478d8dca843979efdab81e33ce8
yuchen <373428582@qq.com>
2020-11-16 18:18:17 +0800

    前端，框架搭建、登录页（权限控制路由），首页框

---
+++ ugly-web-front/src/views/Dashboard/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="dash">
+    <div class="dashRow">
+      <swiper-img class="dashRowSwiper"></swiper-img>
+    </div>
+    <div class="dashYuchen">雨辰</div>
+    <!-- 空白占位，方便后续内容拓展，同时防止页面下方被hidden -->
+    <div class="dashBlock"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import SwiperImg from './components/SwiperImg.vue'
+
+@Component({
+  components: { 
+    SwiperImg
+  }
+})
+export default class DashBoard extends Vue {
+  
+}
+</script>
+
+<style lang="less" scoped>
+.dash {
+  width: 100%;
+  // border: 2px solid blue;
+  // height: 100%;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-around;
+  align-items: center;
+  margin: 30px auto;
+  font-size: 1.5rem;
+  &Yuchen{
+    color:red;
+  }
+  &Row {
+    width: 100%;
+    margin-bottom: 40px;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-items: center;
+    // border: 2px solid red;
+
+    &Swiper {
+      width: 35%;
+      margin: 20px 0;
+    }
+
+    @media screen and (max-width:1000px) {
+      &Item {
+        width: 100%;
+        margin: 10px 0;
+      }
+
+      &Swiper {
+        width: 100%;
+      }
+
+      &Clock {
+        width: 100%;
+      }
+    }
+  }
+
+  &Block {
+    flex: 0 0 50px;
+  }
+}
+</style>
---
+++ ugly-web-front/src/views/Stock/StockCheck/StockCheck.vue
@@ -0,0 +1,40 @@
+<!--  -->
+<template>
+<div class=''>
+    <div>库存信息查询</div>
+</div>
+</template>
+
+<script>
+export default {
+data() {
+return {
+
+};
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+updated() {}, //生命周期 - 更新之后
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+components: {},//组件注册
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
